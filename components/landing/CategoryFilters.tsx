11fb4c5e86eb5ecc6db93b8d2a430cf0aca0239e
Ayo <ayoinspired@gmail.com>
2022-10-21 10:28:54 +0100

    Added filters on categories

---
+++ components/landing/CategoryFilters.tsx
@@ -0,0 +1,38 @@
+import { categories } from "../../utils/data";
+import { VscListFilter } from "react-icons/vsc";
+
+type Props = {
+	category: string;
+	setCategory: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function CategoryFilters({ category, setCategory }: Props) {
+	return (
+		<div className="side-pad flex items-center">
+			<div className="flex items-center ovs h-24 sticky top-[75px] bg-white shadow-xs z-10">
+				{categories.map(({ imgPath, title }: FilterItem) => (
+					<div
+						onClick={() => setCategory(title)}
+						key={title}
+						className="flexed flex-col mr-8 group curp transition-all">
+						<img width={25} src={imgPath} alt={title} />
+						<p
+							className={`capitalize whitespace-nowrap text-xs mt-1 pb-2 group-hover:border-b-2  transition-all ${
+								category === title
+									? "font-semibold border-b-2 border-gray-600 cursor-default"
+									: "group-hover:border-gray-300"
+							}`}>
+							{title}
+						</p>
+					</div>
+				))}
+			</div>
+			<div className="flex items-center p-3 rounded-xl border-2 border-gray-300 ml-4 font-semibold curp">
+				<VscListFilter />
+				<p className="text-sm ml-2">Filters</p>
+			</div>
+		</div>
+	);
+}
+
+export default CategoryFilters;
