0f5db34e1667c1f10a82608e948d58e030b139df
Ayo <ayoinspired@gmail.com>
2022-10-21 13:14:41 +0100

    Minor fix

--- components/landing/ShowMapButton.tsx
+++ components/landing/ShowMapButton.tsx
@@ -2,8 +2,8 @@ import { BsMapFill } from "react-icons/bs";
 
 function ShowMapButton() {
 	return (
-		<div className="hidden lg:flexed sticky bottom-24 h-full z-20">
-			<button className="rounded-3xl bg-gray-800 mx-auto l active:scale-90 transition-all px-5 py-2.5 text-white text-sm flex items-center">
+		<div className="hidden lg:flexed sticky bottom-24 h-full z-20 ">
+			<button className="rounded-3xl bg-gray-800 mx-auto l active:scale-90 transition-all px-5 py-2.5 text-white text-sm flex items-center hover:scale-105 hover:shadow-xl">
 				<span>Show map</span>
 				<BsMapFill className="ml-2 mt-[2px]" />
 			</button>
